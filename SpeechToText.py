f99752679c0e5b38f5f18c837be356fb9150bd68
1510Jeet <1510jeet@gmail.com>
2024-06-28 09:55:07 +0530

    Commiting fastapi file and audio file for testing

--- SpeechToText.py
+++ SpeechToText.py
@@ -11,12 +11,12 @@ from faster_whisper import WhisperModel
 model_size = "large-v3"
 
 # Run on GPU with FP16
-model = WhisperModel(model_size, device="cuda", compute_type="float16")
+# model = WhisperModel(model_size, device="cuda", compute_type="float16")
 
 # or run on GPU with INT8
 # model = WhisperModel(model_size, device="cuda", compute_type="int8_float16")
 # or run on CPU with INT8
-# model = WhisperModel(model_size, device="cpu", compute_type="int8")
+model = WhisperModel(model_size, device="cpu", compute_type="int8")
 
 #to avoid this error: Initializing libiomp5md.dll, but found libiomp5md.dll already initialized. 
 import os
